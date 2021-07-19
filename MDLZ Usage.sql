e1cab86770db518a38e8495d4083906be7915564
brheid <brheid>
2021-07-19 15:44:50 +0000

    account setup

--- MDLZ Usage.sql
+++ MDLZ Usage.sql
@@ -1,20 +1,20 @@
 -- Databricks notebook source
 
-Create or replace temporary view v_zebraDbus AS
+Create or replace temporary view v_mdlzDbus AS
 Select date, yyyymm, monthName, cloudType, workloadType, sfdcAccountName, workspaceId, workspaceName, workspaceStatus, shardName, shardType, nodeTypeName, clusterId, deltaDbus, nonDeltaDbus, dbus, listPrice
 From prod_ds.workloads_cluster_agg
-Where sfdcAccountId = "0016100000bygZdAAI" and Date > date('2021-01-01')
+Where sfdcAccountId = "0016100000U5Dp7AAF" and Date > date('2021-01-01')
 
 -- COMMAND ----------
 
 
-Select * From v_zebraDbus Limit 10
+Select * From v_mdlzDbus Limit 10
 
 -- COMMAND ----------
 
 
 Select sfdcAccountName, cloudType, listPrice, Sum(deltaDbus), sum(nonDeltaDbus), sum(dbus), sum(dbus*listPrice) as dollarDbus
-From v_ZebraDbus
+From v_mdlzDbus
 --Where monthName = "JUNE"
 Where datediff(now(),date)<30
 Group by sfdcAccountName, cloudType, listPrice
