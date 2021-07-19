d1b5d2b388cb15f338c9564c76464568abd23996
brheid <brheid>
2021-07-19 15:40:48 +0000

    Changed to SQL

--- Usage Template.sql
+++ Usage Template.sql
@@ -1,14 +1,17 @@
-# Databricks notebook source
+-- Databricks notebook source
+
 Create or replace temporary view v_zebraDbus AS
 Select date, yyyymm, monthName, cloudType, workloadType, sfdcAccountName, workspaceId, workspaceName, workspaceStatus, shardName, shardType, nodeTypeName, clusterId, deltaDbus, nonDeltaDbus, dbus, listPrice
 From prod_ds.workloads_cluster_agg
 Where sfdcAccountId = "0016100000bygZdAAI" and Date > date('2021-01-01')
 
-# COMMAND ----------
+-- COMMAND ----------
+
 
 Select * From v_zebraDbus Limit 10
 
-# COMMAND ----------
+-- COMMAND ----------
+
 
 Select sfdcAccountName, cloudType, listPrice, Sum(deltaDbus), sum(nonDeltaDbus), sum(dbus), sum(dbus*listPrice) as dollarDbus
 From v_ZebraDbus
